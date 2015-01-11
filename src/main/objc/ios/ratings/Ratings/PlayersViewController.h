7e9c25e30f703715b84fbe74f216cb21c76bb122
Prez Cannady <revprez@opencorrelate.org>
2015-01-11 18:03:32 -0500

    Ratings UI cleanup

--- src/main/objc/ios/ratings/Ratings/PlayersViewController.h
+++ src/main/objc/ios/ratings/Ratings/PlayersViewController.h
@@ -10,5 +10,6 @@
 #import "PlayerDetailsViewController.h"
 
 @interface PlayersViewController : UITableViewController<PlayerDetailsViewControllerDelegate>
+
 @property (nonatomic, strong) NSMutableArray *players;
 @end
